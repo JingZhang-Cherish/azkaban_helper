cfee13edb51afb2c225503fdd3094033abca8524
JingZhang-Cherish <cherish244612023@gmail.com>
2021-09-23 21:06:32 +0800

    v1.0 accomplishment

--- genarate_flow.py
+++ genarate_flow.py
@@ -1,48 +1,58 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+import collections
+import os
 import sys
-
+import shutil
 import xlrd
 
+import yamlparser
+
 '''
-project_name	flow_name	flow_desc	flow_configs	job_name	job_comment	type	command	dependOn
-ods	ODS_USER_INFO	用户信息	base_dir=/home/pg/ods;yxj=100;retries=3	jobA	作业A	command	sh ${base_dir}/user_info.sh	
-ods	ODS_USER_INFO	用户信息	base_dir=/home/pg/ods;yxj=100;retries=4	jobB	作业B	command	sh ${base_dir}/step2.sh	jobA
-ods	ODS_USER_INFO	用户信息	base_dir=/home/pg/ods;yxj=100;retries=5	jobC	作业C	command	sh ${base_dir}/step2.sh	jobB,JobA
+ check dependency jobs whether exits in above jobs or not
 '''
 
 
-def parse_flows():
-    flow_list = {}
-    sheet = xl.sheet_by_name('flows')
-    for i in range(1, sheet.nrows):
-        line = sheet.row_values(i)
+def check_job(flow, depend_jobs):
+    exists_jobs = []
+    for job in flow['nodes']:
+        exists_jobs.append(job.get('name'))
+    return set(depend_jobs).issubset(exists_jobs)
+
+
+'''
+check some cell value whether is null or not
+'''
+
+
+def null(var, desc):
+    if len(var.strip()) == 0:
+        raise Exception('The value ' + desc + 'is required')
+
+
+'''
+parse jobs and flows from excel 
+'''
+
+
+def parse_flows(sheet_name):
+    flow_list = collections.OrderedDict()
+    sheet1 = xl.sheet_by_name(sheet_name)
+    for i in range(1, sheet1.nrows):
+        line = sheet1.row_values(i)
         if len(line) == 0:
             pass
-        flow = flow_list.get(line[1])
-        # 从flow_list中取出flow
-        if flow is None:
-            flow = {}
-        config = flow.get('config')
-        if config is None:
-            config = {}
-        if line[3] is not None:
-            for conf in line[3].strip().split(','):
-                print(conf)
-                cp = conf.strip().split('=')
-                if cp == '':
-                    pass
-                config[cp[0]] = cp[1]
+        # get a flow from flow_list
+        flow = flow_list.get(line[1], collections.OrderedDict())
+        config = parse_flow_config(flow, i, line)
         if config is not None:
             flow['config'] = config
 
         nodes = flow.get('nodes')
         if nodes is None:
             nodes = []
-        job_name = line[4].strip()
-        job_type = line[6].strip()
-        job_config = {'command': line[7].strip()}
-        job = {'name': job_name, 'type': job_type, 'config': job_config}
+
+        job = parse_job(flow, line)
         nodes.append(job)
         flow['nodes'] = nodes
 
@@ -50,12 +60,99 @@ def parse_flows():
     return flow_list
 
 
+'''
+parse flow config from excel column
+'''
+
+
+def parse_flow_config(flow, i, line):
+    config = flow.get('config', collections.OrderedDict())
+    if len(config) != 0:
+        return config
+    if line[3] is not None:
+        for conf in line[3].strip().split('|'):
+            cp = conf.strip().split('=')
+            if cp == '':
+                pass
+            config[cp[0]] = cp[1]
+    else:
+        print('location the ' + str(i) + 'th row: flow_configs is null ')
+    return config
+
+
+'''
+parse jobs config from excel column
+'''
+
+
+def parse_job(flow, line):
+    job_name, job_type = check_null(line)
+    job = collections.OrderedDict()
+    job['name'] = job_name
+    job['type'] = job_type
+    depend_jobs = []
+    if line[8] is not None and line[8].strip() != '':
+        depend_jobs = line[8].strip().split('|')
+    if len(depend_jobs) != 0:
+        job['dependsOn'] = depend_jobs
+        if not check_job(flow, depend_jobs):
+            raise Exception(
+                'job ' + line[1] + '\'s depends: ' + line[8] + ' On not exist in above jobs')
+    job_config = {'command': line[7]}
+    job['config'] = job_config
+    return job
+
+
+'''
+check null parameter from excel column
+'''
+
+
+def check_null(line):
+    job_name = line[4].strip()
+    null(job_name, 'job_name')
+    job_type = line[6].strip()
+    null(job_type, 'job_type')
+    return job_name, job_type
+
+
+def handle_dir(base_dir, sheet_name):
+    try:
+        shutil.rmtree(base_dir)
+    except FileNotFoundError:
+        pass
+    os.mkdir(base_dir)
+    # generate azkaban version describe file
+    with open(base_dir + os.sep + sheet_name + '.project', 'w', encoding='utf-8') as version_file:
+        version_file.write('azkaban-flow-version: 2.0')
+        version_file.close()
+
+
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) < 1:
-        print('please specified the execl file path')
+    if len(args) < 2:
+        print('python generate_flow.py excel_path dir_name')
+        print('please specified the excel file path and the base directory to save flow files ')
         sys.exit(-1)
+    excel_file = args[1]
+    if os.path.exists(excel_file) is None:
+        print(excel_file, 'is not exists')
+        sys.exit(-2)
+    save_dir = args[2]
+    if os.path.isdir(save_dir) is None:
+        print(save_dir, 'is not exists')
+        os.mkdir(save_dir)
 
-    xl = xlrd.open_workbook(args[1])
-    flows = parse_flows()
-    print(flows)
+    xl = xlrd.open_workbook(excel_file)
+    flow_sheets = xl.sheet_names()[1:]
+
+    for sheet in flow_sheets:
+        flows = parse_flows(sheet)
+        project_dir = save_dir + os.sep + sheet
+        handle_dir(project_dir, sheet)
+        for f in flows:
+            flow_file = project_dir + os.sep + f + '.flow'
+            print(flow_file)
+            with open(flow_file, 'w', encoding='utf-8') as output:
+                yamlparser.ordered_yaml_dump(flows[f], output)
+                output.close()
--- main.py
+++ main.py
@@ -4,39 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import json
 
-import xlrd
+from collections import OrderedDict
 import yaml
-
-from Job import Job
-
-
-def open_excel(filename):
-    xl = xlrd.open_workbook(filename)
-    for i in range(0, xl.nsheets):
-        sheet = xl.sheet_by_index(i)
-        print(sheet.name)
-        # print(sheet.nrows)
-        # print(sheet.ncols)
-        sheet = xl.sheet_by_index(0)
-        content = sheet.row_values(0)
-        with open("G:\\tmp\\myPython\\test.yaml", 'w') as f:
-            yaml.dump(content, f)
 
 
 if __name__ == '__main__':
-    fi = "G:\\tmp\\myPython\\test.xlsx"
-    open_excel(fi)
-    jobs = []
-    job1 = Job('job1', 'cc')
-    job2 = Job('job2', 'command')
-    job2.set_command("echo $dt")
-    job1.set_command("echo 123")
-    job1.set_depends_on(job2.job_name)
-
-    with open('G:\\tmp\\myPython\\azkaban\\test.flow', 'r', encoding='utf8') as f:
-        data = yaml.load(f, Loader=yaml.Loader)
-        print(data)
-        f.close()
-    with open('G:\\tmp\\myPython\\azkaban\\test1.flow', 'w', encoding='utf8') as f:
-        yaml.dump(data, f)
-        f.close()
+    print(1)
